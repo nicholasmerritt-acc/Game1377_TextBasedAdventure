d861d7452c17d782dcdafbdd5c982e373abe53ff
UtsabKDas <37959749+UtsabKDas@users.noreply.github.com>
2026-06-12 23:47:16 -0500

    Updated TextBasedAdventure to use a 2D dungeon
    Changed tileNames to a 2D array and added elements to fill out the dungeon
    Changed playerIndex to use two indices, playerRow and playerCol. Updated areas appropriately to use both indices for looking in the array.
    Added W and S input to handle changing the playerCol index to navigate up and down.
    Changed A and D to handle playerRow index.
    Modified bounds check to use both dimensions of the array.

--- Assets/Scripts/TextBasedAdventure.cs
+++ Assets/Scripts/TextBasedAdventure.cs
@@ -2,42 +2,55 @@ using UnityEngine;
 
 public class TextBasedAdventure : MonoBehaviour
 {
-    string[] tileNames = { "Dark Cave", "Mossy Tunnel", "Crystal Room" };
+    string[,] tileNames = { { "Dark Cave",      "Mossy Tunnel",     "Crystal Room" },
+                            { "Bone Chamber",   "Flooded Hall",     "Iron Gate"    },
+                            { "Goblin Den",     "Armory",           "Throne Room"  }
+                            };
+    int playerRow = 0;
+    int playerCol = 0;
 
-    int playerIndex = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("You are in: " + tileNames[playerIndex]);
+        Debug.Log("You are in: " + tileNames[playerRow, playerCol]);
     }
 
     // Update is called once per frame
     void Update()
     {
-        int newIndex = playerIndex;
-        if(Input.GetKeyDown(KeyCode.D))
+        int newRow = playerRow;
+        int newCol = playerCol;
+        if (Input.GetKeyDown(KeyCode.D))
         {
-            newIndex++;
+            newRow++;
         }
-        else if(Input.GetKeyDown(KeyCode.A))
+        else if (Input.GetKeyDown(KeyCode.A))
         {
-            newIndex--;
+            newRow--;
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            newCol++;
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            newCol--;
         }
         else
         {
             return;
         }
-
-        if (newIndex >= 0 && newIndex < tileNames.Length)
+        if ((newRow >= 0 && newRow < tileNames.GetLength(0)) && (newCol >= 0 && newCol < tileNames.GetLength(1)))
         {
-            playerIndex = newIndex;
+            playerRow = newRow;
+            playerCol = newCol;
         }
         else
         {
             Debug.Log("Can't go that way");
         }
-        Debug.Log("You are in: " + tileNames[newIndex]);
+        Debug.Log("You are in: " + tileNames[playerRow, playerCol]);
 
-        
+
     }
 }
